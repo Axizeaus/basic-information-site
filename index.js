be6935971a65dec1032f7b8dca0de0542ee8fc29
Axizeaus <axizeaus@gmail.com>
2023-08-21 14:19:07 +0630

    finished the project, met the basic requirements

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const path = require("node:path");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  fs.readFile(path.join(__dirname, "index.html"), (err, content) => {
-    if (err) throw err;
-    res.writeHead(200, { "Content-type": "text/html" });
-    res.end(content);
+  // build the path
+  const filePath = path.join(
+    __dirname,
+    req.url === "/" ? "index.html" : `${req.url}.html`
+  );
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        fs.readFile(path.join(__dirname, "404.html"), (err, content) => {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(content, "utf-8");
+        });
+      } else {
+        res.writeHead(500);
+        res.end(`server error : ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content, "utf-8");
+    }
   });
 });
 
