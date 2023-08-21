7463ac2350974b00f48c1919f1919d750dcf7b60
Axizeaus <axizeaus@gmail.com>
2023-08-21 13:39:30 +0630

    it shows index html now

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const http = require("http");
+const http = require("node:http");
+const path = require("node:path");
+const fs = require("node:fs");
 
-http
-  .createServer((req, res) => {
-    // write response
-    res.write("Hello World");
-    res.end();
-  })
-  .listen(8080, () => console.log("server running"));
+const server = http.createServer((req, res) => {
+  fs.readFile(path.join(__dirname, "index.html"), (err, content) => {
+    if (err) throw err;
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(content);
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => console.log("server running on port: " + PORT));
